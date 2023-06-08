9e6b995c15475b8b84963ed9747fc1daebe4b966
unknown <mcmp3585@gmail.com>
2023-06-08 18:37:13 -0300

    aprendiendo objetos

---
+++ src/Entidad/Persona.java
@@ -0,0 +1,15 @@
+package Entidad;
+
+public class Persona {
+    
+    public String nombre;
+    public int edad;
+    public String apellido;
+    public Persona(String nombre, int edad, String apellido) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.apellido = apellido;
+    }
+}
+
+
---
+++ src/maopoo/EjMainMAOPOO.java
@@ -0,0 +1,9 @@
+package maopoo;
+
+import Entidad.Persona;
+
+public class EjMainMAOPOO {
+    public static void main(String[] args) {
+        Persona mama =  new Persona("Carmela", 38, "Márquez");
+        Persona papa =  new Persona("Jan",34,"Schroer");
+}
